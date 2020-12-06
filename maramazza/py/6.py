9a1e454ba088eb1ca9978d9bbbd2ca02123706be
Matteo Mazzoli <matteo.mazzoli@iseo.com>
2020-12-06 10:05:57 +0100

    Maramazza commit

--- maramazza/py/6.py
+++ maramazza/py/6.py
@@ -1,32 +1,31 @@
 import re
 from helpers import load_input
 
-
-def sum_groups(answers):
+def sumGroups(values):
     sum = 0
-    for group in answers.split('\n\n'):
-        g_answ = set()
+    for group in values.split('\n\n'):
+        gAnsw = set()
         for answ in group:
             if re.match(r'[a-z]', answ):
-                g_answ.add(answ)
-        sum += len(g_answ)
+                gAnsw.add(answ)
+        sum += len(gAnsw)
     return sum
 
 
-def sum_only_yes(answers):
+def sumOnlyYES(values):
     sum = 0
-    for group in answers.split('\n\n'):
+    for group in values.split('\n\n'):
         candidates = []
         persons = group.splitlines()
         for person in persons:
-            p_answ = set([char for char in person])
-            candidates.append(p_answ)
+            pAnsw = set([char for char in person])
+            candidates.append(pAnsw)
         common = set.intersection(*candidates)
         sum += len(common)
     return sum
 
 
 if __name__ == '__main__':
-    answers = load_input('./../data/6.txt')
-    print(sum_groups(answers))
-    print(sum_only_yes(answers))
+    values = load_input('./../data/6.txt')
+    print(sumGroups(values))
+    print(sumOnlyYES(values))
