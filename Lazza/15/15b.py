e35cf6f0be30944899bb6362e066fccc435c7e15
Andrea Lazzarotto <andrea.lazzarotto@gmail.com>
2020-12-19 01:32:06 +0100

    Giorno 15 Lazza

---
+++ Lazza/15/15b.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+values = [0, 13, 16, 17, 1, 10, 6]
+
+current = values[-1]
+
+positions = {
+    v: [i+1] for i, v in enumerate(values)
+}
+
+iteration = 1
+for v in values:
+    #print(iteration, '->', v)
+    iteration += 1
+
+while iteration <= 30000000:
+    if current not in positions or len(positions[current]) < 2:
+        current = 0
+    else:
+        pos = list(positions[current])
+        current = pos[0] - pos[1]
+    positions[current] = [iteration] + positions.get(current, [])[:1]
+    #print(iteration, '->', current)
+    iteration += 1
+
+print(current)
